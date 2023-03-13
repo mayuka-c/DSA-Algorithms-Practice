f78de096030d1bed262f7a5aa002184ae6acdbd1
mayuka-c <mayuka-c@hpe.com>
2023-03-13 13:24:48 +0530

    GCD, LCM, Zeroes

---
+++ Day1/Basic_Maths/gcd.cpp
@@ -0,0 +1,50 @@
+// To find GCD of 2 numbers using Euclid's algorithm
+
+#include <iostream>
+
+using namespace std;
+
+int gcdV1(int a, int b) {
+
+    // using repeatitive subtraction
+    while (a != b) {
+        if(a > b) {
+            a = a-b;
+        } else {
+            b = b-a;
+        }
+    }
+
+    return a;
+}
+
+// optimized method
+int gcdV2(int a, int b) {
+
+    if (b == 0) {
+        return a;
+    }
+
+    // using modulo division
+    return gcdV2(b, a%b);
+}
+
+int main() {
+
+    int a, b = 0;
+
+    cout << "Enter 2 numbers to find gcd" << endl;
+    cin >> a >> b;
+
+    int res = gcdV2(a, b);
+    cout << "Using modulo division method" << endl;
+    cout << "GCD(" << a << "," << b << ") = " << res << endl;
+    cout << endl;
+
+    res = gcdV1(a, b);
+    cout << "Using repeatitive subtraction method" << endl;
+    cout << "GCD(" << a << "," << b << ") = " << res << endl;
+    cout << endl;
+
+    return 0;
+}
--- Day1/Basic_Maths/trailing_zeros.cpp
+++ Day1/Basic_Maths/trailing_zeros.cpp
@@ -1,1 +1,28 @@
-//
+// To find all the trailing zeros in a factorial
+// Here we count it based on number of 5's in given factorial
+
+#include <iostream>
+
+using namespace std;
+
+int findTrailingZeros(int n) {
+
+    int zerosCount = 0;
+
+    for (int i=5; i<=n; i=i*5) {
+        zerosCount += n/i;
+    }
+
+    return zerosCount;
+}
+
+int main() {
+    int n;
+    cout <<"Enter a number: "<< endl;
+    cin >> n;
+
+    int zerosCount = findTrailingZeros(n);
+    cout << "Number of trailing zeros: " << zerosCount << endl;
+
+    return 0;
+}
